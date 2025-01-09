97cb798cef216ba850254abf32852bf648a4f19b
AdrienMertens <mertensadrien23@hotmail.be>
2025-01-09 14:07:10 +0100

    exo 1.3 ok

---
+++ afficherDuTexte.py
@@ -0,0 +1,13 @@
+"""Afficher du texte"""
+
+# 1
+print("Hello world")  # Affiche "Hello world".
+
+# 2
+print("Aujourd'hui")  # Affiche "Aujourd'hui". Les guillemets doubles permettent d'inclure l'apostrophe sans problème.
+
+# 3
+print('c\'est "dommage"!')  # Utilisation d'un guillemet simple avec une échappement pour l'apostrophe, et des guillemets doubles pour "dommage".
+
+# 4
+print("Hum \\O/")  # Utilisation d'un double antislash `\\` pour afficher correctement le caractère `\`.
